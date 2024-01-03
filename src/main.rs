450ad2a18b54254180a186dda99c69586880a7c9
Bankov Andrey "Rottenfront <47115385+Rottenfront@users.noreply.github.com>
2024-01-03 10:54:33 +0300

    Event loop went to new thread

--- src/main.rs
+++ src/main.rs
@@ -4,73 +4,97 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::cmp::min;
-use std::path::PathBuf;
-use std::sync::Arc;
-use futures::executor;
-use futures::prelude::*;
+use futures::{executor, prelude::*};
+use std::time::Duration;
+use std::{cmp::min, path::PathBuf, sync::Arc, thread};
 
 use flo_draw::*;
-use flo_draw::{binding::{bind, BindRef, Binding, Bound, MutableBound}, canvas::*};
-use flo_draw::winit::window::{CursorIcon, Theme, WindowLevel};
+use flo_draw::{
+    binding::{bind, BindRef, Binding, Bound, MutableBound},
+    canvas::*,
+    winit::{
+        event::{MouseButton, MouseScrollDelta, TouchPhase},
+        window::{CursorIcon, Theme, WindowLevel},
+    },
+};
 
+#[derive(Debug, Clone)]
 enum SpanType {
     Text,
     Comment,
     Keyword,
 }
 
+#[derive(Debug, Clone)]
 struct EditorState {
     file: Vec<String>,
     name: String,
     path: Option<PathBuf>,
-    scroll: (f64, f64),
+    scroll: (f32, f32),
     spans: Vec<(usize, usize, usize, usize, SpanType)>,
     cursors: Vec<(usize, usize, usize, usize)>,
 }
 
+#[derive(Debug, Clone)]
 struct FSItem {
     name: String,
     is_folded: bool,
     level: usize,
 }
 
+#[derive(Debug, Clone)]
 struct FSState {
     root: PathBuf,
     items: Vec<FSItem>,
 }
 
+#[derive(Debug, Clone)]
 enum LeftItem {
     None,
     FS,
 }
 
+#[derive(Debug, Clone)]
 struct LeftBar {
     enabled: LeftItem,
-    fs: FSState,
+    // fs: FSState,
 }
 
+#[derive(Debug, Clone)]
 enum RightItem {
     None,
 }
 
+#[derive(Debug, Clone)]
 struct RightBar {
     enabled: RightItem,
 }
 
+#[derive(Debug, Clone)]
 enum BottomItem {
     None,
     Terminal,
 }
 
-struct TermState {
-}
+#[derive(Debug, Clone)]
+struct TermState {}
 
+#[derive(Debug, Clone)]
 struct BottomBar {
     enabled: BottomItem,
-    terminal: TermState,
+    // terminal: TermState,
 }
 
+#[derive(Debug, Clone)]
+struct InputState {
+    pointer_pos: Binding<(f32, f32)>,
+    pointer_key: Binding<Option<MouseButton>>,
+    is_scroll_started: Binding<bool>,
+    prev_scroll_delta: Binding<(f32, f32)>,
+    scroll_delta: Binding<(f32, f32)>,
+}
+
+#[derive(Debug, Clone)]
 struct State {
     size: Binding<(u64, u64)>,
     min_size: Binding<Option<(u64, u64)>>,
@@ -89,19 +113,21 @@ struct State {
     theme: Binding<Option<Theme>>,
     cursor_position: Binding<(u64, u64)>,
     cursor_icon: Binding<MousePointer>,
+    is_exit_emmited: Binding<bool>,
+    is_needed_redraw: Binding<bool>,
+
+    input_state: InputState,
 
     editors: Vec<EditorState>,
     enabled_editor: Option<usize>,
 
-    left_bound: f64,
-    right_bound: f64,
-    bottom_bound: f64,
+    left_bound: f32,
+    right_bound: f32,
+    bottom_bound: f32,
 
     left_items: LeftBar,
     right_items: RightBar,
     bottom_items: BottomBar,
-
-    canvas: DrawingTarget,
 
     serif_font: Arc<CanvasFontFace>,
     serif_font_size: f32,
@@ -127,25 +153,31 @@ struct ColorTheme {
     sidebar_button_back: Color,
     sidebar_button_hover: Color,
     sidebar_button_active: Color,
-
 }
 
 impl EditorState {
     fn draw(&self, gc: &mut Vec<Draw>, x1: f32, y1: f32, x2: f32, y2: f32, metrics: &FontMetrics) {
-        let first_line = (self.scroll.1 as f32 / metrics.height) as usize;
-        let last_line = ((self.scroll.1 + (y1 - y2)) as f32 / metrics.height) as usize + 1;
-
+        let first_line = (self.scroll.1 / metrics.height) as usize;
+        let last_line = ((self.scroll.1 + (y2 - y1)) / metrics.height) as usize + 1;
+        // println!("{first_line}, {last_line}");
+        // gc.unclip();
         gc.new_path();
         gc.rect(x1, y1, x2, y2);
         gc.clip();
 
-
-        let delta_y = self.scroll.1 - metrics.height * first_line;
-        let y2 = y2 + delta_y;
+        let delta_y = self.scroll.1 - metrics.height * (first_line as f32);
+        let y2 = y2 + delta_y - metrics.height;
+        gc.fill_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
         for i in first_line..min(last_line, self.file.len()) {
-            gc.fill_text(self.file[i].as_str(), x1, y2 + (i - first_line) as f32 * metrics.height, metrics.em_size, Color::Rgba(0.0, 0.0, 0.0, 1.0));
+            gc.begin_line_layout(
+                x1,
+                y2 - (i - first_line) as f32 * metrics.height,
+                TextAlignment::Left,
+            );
+            gc.layout_text(MONOSPACE_FONT, self.file[i].clone());
+            gc.draw_text_layout();
+            // gc.fill_text(self.file[i].as_str(), x1, y2, metrics.em_size, Color::Rgba(0.0, 0.0, 0.0, 1.0));
         }
-
 
         gc.unclip();
     }
@@ -165,69 +197,65 @@ impl State {
         gc.define_font_data(MONOSPACE_FONT, Arc::clone(&self.monospace_font));
         gc.set_font_size(SERIF_FONT, self.serif_font_size);
         gc.set_font_size(MONOSPACE_FONT, self.monospace_font_size);
+        self.draw_editor(gc);
+        let (x, y) = self.input_state.pointer_pos.get();
+        // Draw on layer 1 to avoid disrupting the image underneath
+        gc.layer(LayerId(3));
+        gc.clear_layer();
+
+        gc.new_path();
+        gc.circle(x as _, y as _, 20.0);
+
+        gc.stroke_color(Color::Rgba(0.1, 0.1, 0.1, 0.8));
+        gc.line_width_pixels(3.0);
+        gc.stroke();
+
+        gc.stroke_color(Color::Rgba(0.6, 0.9, 0.6, 0.8));
+        gc.line_width_pixels(2.0);
+        gc.stroke();
     }
 
     fn draw_editor(&self, gc: &mut Vec<Draw>) {
         let (width, height) = self.size.get();
-        let width = width as _;
-        let height = height as _;
+        let width = width as f32;
+        let height = height as f32;
 
-        let metrics = self.monospace_font.font_metrics(self.monospace_font_size).unwrap();
+        let metrics = self
+            .monospace_font
+            .font_metrics(self.monospace_font_size)
+            .unwrap();
 
-        let x1 = LEFT_BAR_WIDTH + match self.left_items.enabled {
-            LeftItem::None => 0.0,
-            _ => self.left_bound,
-        };
-        let y1 = BOTTOM_BAR_HEIGHT + match self.bottom_items.enabled {
-            BottomItem::None => 0.0,
-            _ => self.bottom_bound,
-        };
-        let x2 = width - RIGHT_BAR_WIDTH - match self.right_items.enabled {
-            RightItem::None => 0.0,
-            _ => self.right_bound,
-        };
+        let x1 = LEFT_BAR_WIDTH
+            + match self.left_items.enabled {
+                LeftItem::None => 0.0,
+                _ => self.left_bound,
+            };
+        let y1 = BOTTOM_BAR_HEIGHT
+            + match self.bottom_items.enabled {
+                BottomItem::None => 0.0,
+                _ => self.bottom_bound,
+            };
+        let x2 = width
+            - RIGHT_BAR_WIDTH
+            - match self.right_items.enabled {
+                RightItem::None => 0.0,
+                _ => self.right_bound,
+            };
         let y2 = height - UPPER_BAR_HEIGHT;
 
-        gc.new_path();
-        gc.rect(x1, y1, x2, y2);
-        gc.clip();
+        // gc.new_path();
+        // gc.rect(x1, y1, x2, y2);
+        // gc.clip();
 
         match self.enabled_editor {
-            None => {},
+            None => {}
             Some(idx) => {
                 self.editors[idx].draw(gc, x1, y1, x2, y2 - UPPER_BAR_HEIGHT, &metrics);
-            },
+            }
         }
 
-        gc.unclip()
+        // gc.unclip()
     }
-}
-
-fn draw(gc: &mut Vec<Draw>, width: f32, height: f32, serif: &Arc<CanvasFontFace>, monospace: &Arc<CanvasFontFace>) {
-    // Load the fonts
-    gc.define_font_data(FontId(1), Arc::clone(&serif));
-    gc.define_font_data(FontId(2), Arc::clone(&monospace));
-    gc.set_font_size(FontId(1), 14.0);
-    gc.set_font_size(FontId(2), 14.0);
-
-
-    gc.new_path();
-    gc.rect(0.0, 0.0, width, 50 as _);
-    gc.rect(0.0, height - 50.0, width, height);
-
-    gc.fill_color(Color::Rgba(1.0, 1.0, 0.8, 1.0));
-    gc.fill();
-
-    // Draw some text with layout in these fonts
-    gc.fill_color(Color::Rgba(0.0, 0.0, 0.6, 1.0));
-
-    gc.begin_line_layout(14.0, 700.0 - 100.0, TextAlignment::Left);
-    gc.layout_text(FontId(1), "It's also possible to alter ".to_string());
-    gc.set_font_size(FontId(1), 36.0);
-    gc.layout_text(FontId(1), "sizes".to_string());
-    gc.set_font_size(FontId(1), 14.0);
-    gc.layout_text(FontId(1), " during layout ".to_string());
-    gc.draw_text_layout();
 }
 
 pub fn main() {
@@ -238,7 +266,7 @@ pub fn main() {
         let size = bind((1024, 768));
         let min_size = bind(None);
         let max_size = bind(None);
-        let title = bind("Mouse tracking".to_string());
+        let title = bind("Fluss".to_string());
         let is_transparent = bind(false);
         let is_visible = bind(true);
         let is_resizable = bind(true);
@@ -272,68 +300,129 @@ pub fn main() {
             cursor_icon: BindRef::from(cursor_icon.clone()),
         };
 
+        let state = State {
+            size,
+            min_size,
+            max_size,
+            title,
+            is_transparent,
+            is_visible,
+            is_resizable,
+            is_minimized,
+            is_maximized,
+            fullscreen,
+            has_decorations,
+            window_level,
+            ime_position,
+            ime_allowed,
+            theme,
+            cursor_position,
+            cursor_icon,
+            is_exit_emmited: bind(false),
+            is_needed_redraw: bind(true),
+
+            editors: vec![EditorState {
+                file: vec![
+                    "fn main() {".to_string(),
+                    "    println!(\"Hello, World!\");".to_string(),
+                    "}".to_string(),
+                ],
+                name: "editor".to_string(),
+                path: None,
+                scroll: (0.0, 0.0),
+                spans: vec![],
+                cursors: vec![],
+            }],
+            enabled_editor: Some(0),
+
+            input_state: InputState {
+                pointer_key: bind(None),
+                pointer_pos: bind((0.0, 0.0)),
+                is_scroll_started: bind(false),
+                prev_scroll_delta: bind((0.0, 0.0)),
+                scroll_delta: bind((0.0, 0.0)),
+            },
+
+            left_bound: 100.0,
+            right_bound: 100.0,
+            bottom_bound: 100.0,
+
+            left_items: LeftBar {
+                enabled: LeftItem::None,
+            },
+            right_items: RightBar {
+                enabled: RightItem::None,
+            },
+            bottom_items: BottomBar {
+                enabled: BottomItem::None,
+            },
+
+            serif_font: serif,
+            serif_font_size: 14.0,
+            monospace_font: monospace,
+            monospace_font_size: 14.0,
+        };
+
+        let events_state = state.clone();
+
         // Create a window and an event queue
         let (canvas, events) = create_drawing_window_with_events(window_props);
-        let (width, height) = size.get();
-        let width = width as f32;
-        let height = height as f32;
-        canvas.draw(|gc| {
-            // Set up the canvas
-            gc.canvas_height(height);
-            gc.center_region(0.0, 0.0, width, height);
-            gc.layer(LayerId(0));
-            draw(gc, width, height, &serif, &monospace);
-        });
         // Track mouse events and render a circle centered on the current position (we use layer 1 for this so we don't have to re-render the whole canvas)
-        executor::block_on(async move {
-            let mut events = events;
+        thread::Builder::new()
+            .name("Event thread".to_string())
+            .spawn(move || {
+                executor::block_on(async move {
+                    let mut events = events;
 
-            // Main event loop
-            while let Some(event) = events.next().await {
-                match event {
-                    DrawEvent::Resized(new_size) => {
-                        // Resize the canvas
-                        size.set((new_size.width as _, new_size.height as _));
-                        let (width, height) = (new_size.width, new_size.height);
-                        canvas.draw(|gc| {
-                            // Clear the canvas and set up the coordinates
-                            gc.clear_canvas(Color::Rgba(1.0, 1.0, 1.0, 1.0));
-                            gc.canvas_height(height as _);
-                            gc.center_region(0.0, 0.0, width as _, height as _);
+                    // Main event loop
+                    while let Some(event) = events.next().await {
+                        match event {
+                            DrawEvent::Resized(new_size) => {
+                                // Resize the canvas
+                                events_state
+                                    .size
+                                    .set((new_size.width as _, new_size.height as _));
+                                events_state.is_needed_redraw.set(true);
+                                // let (width, height) = (new_size.width, new_size.height);
+                            }
 
-                            gc.layer(LayerId(0));
-                            gc.clear_layer();
-                            draw(gc, width as _, height as _, &serif, &monospace);
-                        });
-                    }
+                            // Track any event relating to the pointer
+                            DrawEvent::CursorMoved { state } => {
+                                // Draw a circle at the mouse position
+                                if let Some((x, y)) = state.location_in_canvas {
+                                    events_state.input_state.pointer_pos.set((x as _, y as _));
+                                    events_state.is_needed_redraw.set(true);
+                                }
+                            }
 
-                    // Track any event relating to the pointer
-                    DrawEvent::CursorMoved { state } => {
-                        // Draw a circle at the mouse position
-                        if let Some((x, y)) = state.location_in_canvas {
-                            canvas.draw(|gc| {
-                                // Draw on layer 1 to avoid disrupting the image underneath
-                                gc.layer(LayerId(3));
-                                gc.clear_layer();
+                            DrawEvent::MouseWheel { delta, phase } => {
+                                match phase {
+                                    TouchPhase::Started => {}
+                                    TouchPhase::Ended => {}
+                                    TouchPhase::Moved => {}
+                                    TouchPhase::Cancelled => {}
+                                }
+                                match delta {
+                                    MouseScrollDelta::LineDelta(x, y) => {}
+                                    MouseScrollDelta::PixelDelta(pos) => {}
+                                }
+                            }
 
-                                gc.new_path();
-                                gc.circle(x as _, y as _, 20.0);
-
-                                gc.stroke_color(Color::Rgba(0.1, 0.1, 0.1, 0.8));
-                                gc.line_width_pixels(3.0);
-                                gc.stroke();
-
-                                gc.stroke_color(Color::Rgba(0.6, 0.9, 0.6, 0.8));
-                                gc.line_width_pixels(2.0);
-                                gc.stroke();
-                            });
+                            // Ignore other events
+                            _ => {}
                         }
                     }
-
-                    // Ignore other events
-                    _ => {}
-                }
+                })
+            })
+            .expect("Event thread didn't started");
+        while !state.is_exit_emmited.get() {
+            if state.is_needed_redraw.get() {
+                canvas.draw(|gc| {
+                    state.draw(gc);
+                });
+                state.is_needed_redraw.set(false);
             }
-        })
+            thread::sleep(Duration::from_secs_f32(1.0 / 60.0))
+        }
     });
 }
